638c81879e93f6ebb4a625c9ee2b89e6817053c9
Tim Hoerner <thoerner@gmail.com>
2022-10-03 14:24:20 -0400

    formatting

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Typewriter
-        options={{
-        strings: ['Hello, Welcome to the world of LocalCryptoGod', 'Some cool shit is about to happen'],
-        autoStart: true,
-        loop: true,
-      }}
-    />
-          <a
+          options={{
+            strings: ['Hello, Welcome to the world of LocalCryptoGod', 'Some cool shit is about to happen'],
+             autoStart: true,
+             loop: true,
+            }}
+          />
+        <a
           className="App-link"
           href="https://twitter.com/localcryptogod"
           target="_blank"
