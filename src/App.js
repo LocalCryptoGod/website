835c1fe268ecfe465b28560b365f8704f0add549
Localcrypto <potamus@edu.com>
2022-10-02 20:37:27 -0400

    added typewriter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import logo from './logo.png';
 import './App.css';
+import Typewriter from 'typewriter-effect';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to the world of:
-        </p>
-        <a
+        <Typewriter
+        options={{
+        strings: ['Hello, Welcome to the world of LocalCryptoGod', 'Some cool shit is about to happen.'],
+        autoStart: true,
+        loop: true,
+      }}
+    />
+          <a
           className="App-link"
           href="https://twitter.com/localcryptogod"
           target="_blank"
@@ -17,9 +22,10 @@ function App() {
         >
           LocalCryptoGod
         </a>
-      </header>
+        </header>
     </div>
   );
 }
+
 
 export default App;
